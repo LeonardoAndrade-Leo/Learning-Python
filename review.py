5a0d13ff7a25da552c8ce1b0bda069d14007b98c
Leonardo Andrade Oliveira Santos <leonardo.andradeoliveirasantos@gmail.com>
2025-02-13 12:02:26 -0300

    Created review.py
    I wrote the first part of the content's review

---
+++ review.py
@@ -0,0 +1,15 @@
+# In this review, I'm seeing some concepts as Data Types, variables
+
+# Data Types
+# 1) int: usually numbers without a fractional component, like (1, 2, 3, -5)
+# 2) float: numbers with fractions (1.2, 0.6)
+# 3) bool: it represents true or false values 
+# 4) str: it represents a sequence of characteres, as words, phrases, digits (hello, 44+33,3)
+
+nome= 'leonardo' # Variables: piece of memory where we keep some data. To create a variable, we must follow some naming rules. 
+print(nome) # here, I wrote a command that when executed, the value of the variable 'nome', will appear on the screen
+  
+
+
+
+
