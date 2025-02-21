da4c2868be84647b5eb1b7cc1454715a529e219a
Leonardo Andrade Oliveira Santos <leonardo.andradeoliveirasantos@gmail.com>
2025-02-21 10:53:17 -0300

    I created the file "print.py"
    I saw the basic of the Print using.

---
+++ print.py
@@ -0,0 +1,8 @@
+print('Hello, world! This is the way we print something on the screen')
+
+name='Leonardo'
+middle_name= ' Andrade'
+
+print(name+ middle_name) # we can use 'print' to print variables 
+
+
