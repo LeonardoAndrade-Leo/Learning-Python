275922fe81390cd437e3a84ed5f28504842bfd69
Leonardo Andrade Oliveira Santos <leonardo.andradeoliveirasantos@gmail.com>
2025-02-21 11:07:40 -0300

    Created input.py
    I saw how to receive an information from the user and use it to write a welcoming message

---
+++ input.py
@@ -0,0 +1,8 @@
+nome=input('What is your name?: ')
+
+print('Welcome to our world',nome)
+
+# we can also format the variable to our print, so we don't waste time writing every variable, for example:
+
+print('Welcome {}, we are glad to see you'.format(nome))
+
