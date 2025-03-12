8502c2e925a1dd1cfd6074822533a1246f2eaa38
Leonardo Andrade Oliveira Santos <195787320+LeonardoAndrade-Leo@users.noreply.github.com>
2025-03-12 10:46:36 -0300

    update
    add some information

--- creating_modules_packages.py
+++ creating_modules_packages.py
@@ -1,1 +1,5 @@
 # A module is a Python file that contains reusable functions, classes and variables.
+
+import math # It imports the whole module
+
+from math import pi, cos #It imports just the wanted functions
